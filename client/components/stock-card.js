1253213dd269811af3b5d197240be97a68eec501
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-08-09 14:26:39 -0500

    Route to card

---
+++ client/components/stock-card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+//import {Button} from 'semantic-ui-react'
+
+// const trial = () => {
+//   console.log('You have reach the trial')
+//   return (
+//     <div>
+//       <Button>Click Me</Button>
+//     </div>
+//   )
+// }
+
+export const Stockcard = () => {
+  return (
+    <div>
+      <h1>Hello from Card</h1>
+    </div>
+  )
+}
--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
+import {Stockcard} from './components/stock-card'
 import {me} from './store'
 
 /**
@@ -19,6 +20,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route exact path="/stockcard" component={Stockcard} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
