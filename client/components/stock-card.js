31dcdc53a92298202e8eb2c8570a5f4b539ace0c
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-08-12 17:55:21 -0500

    Bulma packages updated as well as package.json

--- client/components/stock-card.js
+++ client/components/stock-card.js
@@ -1,5 +1,4 @@
 import React from 'react'
-//import {Grid, Image, Container, Icon, Divider} from 'semantic-ui-react'
 
 export const Stockcard = () => {
   return (
@@ -12,42 +11,6 @@ export const Stockcard = () => {
   )
 }
 
-//     <Container>
-//       <Divider />
-//       <Grid>
-//         <Grid.Column width={4}>
-//           <Image src="https://images.theconversation.com/files/38926/original/5cwx89t4-1389586191.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip" />
-//         </Grid.Column>
-//         <Grid.Column width={6}>
-//           <h3>Name of Meme</h3>
-//           <h5>Total Available: 50</h5>
-//           <p>
-// Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-// commodo ligula eget dolor. Aenean massa strong.
-//           </p>
-//           <Icon name="caret down" color="red" size="huge" />
-//           <Icon name="caret up" color="green" size="huge" />
-//         </Grid.Column>
-//       </Grid>
-//       <Divider />
-//     </Container>
-//     //
-
-// <div
-//   className="ui raised very padded text container segment"
-//   style={styles.div}
-// >
-
-// const styles = {
-//   div: {
-//     marginTop: 40,
-//     width: 1000
-//   },
-//   header: {
-//     paddingBottom: 10
-//   },
-//   subtotal: {
-//     textAlign: 'right',
-//     marginRight: 20
-//   }
+// export const Stockcard = () => {
+//   return <button className="button is-danger is-small">Button</button>
 // }
