ad91955bf712b358c6219688b0e9427b3ab9277b
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-08-13 13:57:29 -0500

    Is Logged in toggle page working

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,6 +4,14 @@ import {connect} from 'react-redux'
 import {Link, NavLink} from 'react-router-dom'
 import {logout} from '../store'
 
+const toggleNav = event => {
+  const navbarBurger = event.target
+  const navbarMenu = document.getElementById('navPrimary')
+
+  navbarBurger.classList.toggle('is-active')
+  navbarMenu.classList.toggle('is-active')
+}
+
 const Navbar = ({id, handleLogout, isLoggedIn, isAdmin}) => (
   <nav className="navbar is-primary is-fixed-top">
     <div className="container">
@@ -11,16 +19,26 @@ const Navbar = ({id, handleLogout, isLoggedIn, isAdmin}) => (
         <Link to="/" className="navbar-item has-text-weight-bold">
           Memenomics
         </Link>
-        {/* <div
-          className="navbar-burger burger"
-          onClick={event => toggleNav(event)}
-          data-target="navPrimary"
-        >
-        <span></span>
-        </div> */}
-        <div className="navbar-menu">
-          <div className="navbar-start">
-            <span>Start</span>
+      </div>
+
+      <div
+        className="navbar-burger burger"
+        onClick={event => toggleNav(event)}
+        data-target="navPrimary"
+      >
+        <span />
+      </div>
+
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <div className="field has-addons">
+            <div className="control">
+              {/* <br /> */}
+              <input className="input" type="text" placeholder="Search" />
+            </div>
+            <div className="control">
+              <a className="button is-info">Search</a>
+            </div>
           </div>
         </div>
 
@@ -30,7 +48,7 @@ const Navbar = ({id, handleLogout, isLoggedIn, isAdmin}) => (
               <Link className="navbar-link" to="/user-portfolio">
                 Account
               </Link>
-              <div className="navbar-dropdown">
+              {/* <div className="navbar-dropdown">
                 <NavLink
                   to={`/user/${id}/portfolio`}
                   className="navbar-item"
@@ -40,8 +58,9 @@ const Navbar = ({id, handleLogout, isLoggedIn, isAdmin}) => (
                 </NavLink>
                 <Link to={`user/${id}/offers`} className="navbar-item">
                   Offers
-                </Link>
-                {isAdmin && (
+                </Link> */}
+
+              {/* {isAdmin && (
                   <div>
                     <hr className="navbar-divider" />
                     <NavLink
@@ -52,13 +71,24 @@ const Navbar = ({id, handleLogout, isLoggedIn, isAdmin}) => (
                       Manage
                     </NavLink>
                   </div>
-                )}
-                <hr className="navbar-divider" />
-                <a href="#" onClick={handleLogout} className="navbar-item">
-                  Logout
-                </a>
-              </div>
+                )} */}
+
+              <hr className="navbar-divider" />
+              <a href="#" onClick={handleLogout} className="navbar-item">
+                Logout
+              </a>
             </div>
+            // </div>
+          )}
+          {!isLoggedIn && (
+            <Link to="/login" className="navbar-item">
+              Login
+            </Link>
+          )}
+          {!isLoggedIn && (
+            <Link to="/signup" className="navbar-item">
+              Sign up
+            </Link>
           )}
         </div>
       </div>
@@ -66,74 +96,20 @@ const Navbar = ({id, handleLogout, isLoggedIn, isAdmin}) => (
   </nav>
 )
 
-// const Navbar = ({handleClick, isLoggedIn}) => (
-//   <div>
-//     {isLoggedIn ? (
-//       <div>
-//         {/* The navbar will show these links after you log in */}
-//         <Menu>
-//           <Menu.Item>
-//             <Link to="/landingPage">
-//               <Button primary>Home</Button>
-//             </Link>
-//           </Menu.Item>
-//           <Menu.Item>
-//             <a href="#" onClick={handleClick}>
-//               <Button>Logout</Button>
-//             </a>
-//           </Menu.Item>
-//           <Menu.Item position="center">
-//             <h1>Meme Economy</h1>
-//           </Menu.Item>
-//           <Menu.Item position="right">
-//             <Input
-//               action={{type: 'submit', content: 'Go'}}
-//               placeholder="Navigate to..."
-//             />
-//           </Menu.Item>
-//         </Menu>
-//       </div>
-//     ) : (
-//       <div>
-//         <Menu>
-//           {/* The navbar will show these links before you log in */}
-//           <Menu.Item>
-//             <Link to="/login">
-//               <Button primary>Login</Button>
-//             </Link>
-//           </Menu.Item>
-//           <Menu.Item>
-//             <Link to="/signup">
-//               <Button>Sign up</Button>
-//             </Link>
-//           </Menu.Item>
-//           <Menu.Item position="center">
-//             <h1>Meme Economy</h1>
-//           </Menu.Item>
-//           <Menu.Item position="right">
-//             <Input
-//               action={{type: 'submit', content: 'Go'}}
-//               placeholder="Navigate to..."
-//             />
-//           </Menu.Item>
-//         </Menu>
-//       </div>
-//     )}
-//   </div>
-// )
-
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
+    // isAdmin: !!state.me.isAdmin,
+    //id: state.me.id
   }
 }
 
 const mapDispatch = dispatch => {
   return {
-    handleClick() {
+    handleLogout() {
       dispatch(logout())
     }
   }
@@ -145,61 +121,7 @@ export default connect(mapState, mapDispatch)(Navbar)
  * PROP TYPES
  */
 Navbar.propTypes = {
-  handleClick: PropTypes.func.isRequired,
+  handleLogout: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
+  // isAdmin: PropTypes.bool.isRequired
 }
-
-//Example NavBar with Bulma
-{
-  /* <nav className="navbar is-primary">
-<div className="container">
-  <div className="navbar-brand">
-    <a className="navbar-item has-text-weight-bold" href="#">
-      Meme Logo
-    </a> */
-}
-{
-  //this is an insert for mobile devices with hamburger
-  /* <div className="navbar-burger burger" data-target="navMenu">
-      <span />
-      <span />
-      <span />
-    </div> */
-}
-//   </div>
-//   <div className="navbar-menu" id="navMenu">
-//     <div className="navbar-start">
-//       <div className="navbar-item has-dropdown is-hoverable">
-//         <a href="#" className="navbar-link is-active">
-//           Browse
-//         </a>
-//         <div className="navbar-dropdown">
-//           <a href="#" className="navbar-item">
-//             Wow
-//           </a>
-//           <a href="#" className="navbar-item">
-//             Dang
-//           </a>
-//           <a href="#" className="navbar-item">
-//             Hot damn
-//           </a>
-//         </div>
-//       </div>
-//     </div>
-//     <div className="navbar-end">
-//       <a href="#" className="navbar-item">
-//         Home
-//       </a>
-//       <a href="#" className="navbar-item">
-//         Index
-//       </a>
-//       <a href="#" className="navbar-item">
-//         Indices
-//       </a>
-//       <a href="#" className="navbar-item">
-//         Submit New
-//       </a>
-//     </div>
-//   </div>
-// </div>
-// </nav>
