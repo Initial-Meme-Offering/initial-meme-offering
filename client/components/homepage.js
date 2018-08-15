3858a3f12a945f7eed014b8bdd47bb5e0257bed2
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-08-14 20:19:36 -0500

    Styling of header and footer added

--- client/components/homepage.js
+++ client/components/homepage.js
@@ -10,45 +10,47 @@ import {
 
 const HomePage = () => {
   return (
-    <div>
-      <div className="columns is-multiline is-centered">
-        <div className="column is-one-third below-nav-bar">
-          <figure className="image is-square">
-            <Link to="/allmemes" component={AllMemes}>
-              <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
-            </Link>
-          </figure>
-        </div>
-        <div className="column is-one-third below-nav-bar">
-          <figure className="image is-square">
-            <Link to="/trendingmemes" component={TrendingMemes}>
-              <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
-            </Link>
-          </figure>
-        </div>
-        <div className="column is-one-third below-nav-bar">
-          <figure className="image is-square">
-            <Link to="/indices" component={Indices}>
-              <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
-            </Link>
-          </figure>
-        </div>
-        <div className="column is-one-third below-nav-bar">
-          <figure className="image is-square">
-            <Link to="/searchmemes" component={SearchMemes}>
-              <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
-            </Link>
-          </figure>
-        </div>
-        <div className="column is-one-third below-nav-bar">
-          <figure className="image is-square">
-            <Link to="/purchasememes" component={PurchaseMemes}>
-              <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
-            </Link>
-          </figure>
+    <section id="first-division" className="section is-medium">
+      <div className="container">
+        <div className="columns is-multiline is-centered">
+          <div className="column is-one-third">
+            <figure className="image is-square">
+              <Link to="/allmemes" component={AllMemes}>
+                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+              </Link>
+            </figure>
+          </div>
+          <div className="column is-one-third">
+            <figure className="image is-square">
+              <Link to="/trendingmemes" component={TrendingMemes}>
+                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+              </Link>
+            </figure>
+          </div>
+          <div className="column is-one-third">
+            <figure className="image is-square">
+              <Link to="/indices" component={Indices}>
+                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+              </Link>
+            </figure>
+          </div>
+          <div className="column is-one-third">
+            <figure className="image is-square">
+              <Link to="/searchmemes" component={SearchMemes}>
+                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+              </Link>
+            </figure>
+          </div>
+          <div className="column is-one-third">
+            <figure className="image is-square">
+              <Link to="/purchasememes" component={PurchaseMemes}>
+                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+              </Link>
+            </figure>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
--- client/components/index.js
+++ client/components/index.js
@@ -5,6 +5,7 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
+export {default as Footer} from './footer'
 export {default as UserHome} from './user-home'
 export {Login, Signup} from './auth-form'
 export {default as LandingPage} from './landing-page'
