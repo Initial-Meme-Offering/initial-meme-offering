31b231e97da5ea244848695e09396bc8c0222f11
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-08-15 10:38:06 -0500

    Styling consistency changes

--- client/components/stock-card-small.js
+++ client/components/stock-card-small.js
@@ -1,34 +1,34 @@
 import React from 'react'
 
 //May need to afix cemented size to make sure multiple per row fit
-const SmallStockCard = (props) => {
-  const {memeImage, memeName} = props 
+const SmallStockCard = props => {
+  const {memeImage, memeName} = props
   return (
-    <div className="card below-nav-bar">
-    <div className="card-image">
-      <figure className="image is-4by3">
-        <img src={memeImage} alt="Placeholder image"/>
-      </figure>
-    </div>
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src={memeImage} alt="Placeholder image"/>
-          </figure>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={memeImage} alt="Placeholder image" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src={memeImage} alt="Placeholder image" />
+            </figure>
+          </div>
+          <div className="media-content">
+            <p className="title is-4 meme-font">{memeName}</p>
+            <p className="subtitle is-6 meme-font">@{memeName}</p>
+          </div>
         </div>
-        <div className="media-content">
-          <p className="title is-4 meme-font">{memeName}</p>
-          <p className="subtitle is-6 meme-font">@{memeName}</p>
+        <div className="content meme-font">
+          Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris.
+          <br />
         </div>
       </div>
-      <div className="content meme-font">
-        Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Phasellus nec iaculis mauris.
-        <br/>
-      </div>
     </div>
-  </div>
   )
 }
 
