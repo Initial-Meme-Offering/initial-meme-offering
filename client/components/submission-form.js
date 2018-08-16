68fa174e05e9f390ef48b457e62c8beb8a5b1135
Michael Bush <lil.gordie@gmail.com>
2018-08-16 12:59:42 -0500

    offers display on portfolio page

--- client/components/submission-form.js
+++ client/components/submission-form.js
@@ -2,29 +2,50 @@ import React from 'react'
 import {Field, reduxForm} from 'redux-form'
 import {connect} from 'react-redux'
 
-class SubmissionForm extends React.Component {
-  render() {
-    return (
-      <form>
-        <Field
-          className="field input"
-          name="quantity"
-          component={renderQuantityField}
-          type="text"
-        />
-        <Field
-          className="field input"
-          name="quantity"
-          component={renderQuantityField}
-          type="text"
-        />
-        <Field
-          className="field input"
-          name="quantity"
-          component={renderQuantityField}
-          type="text"
-        />
-      </form>
-    )
+const renderField = ({input, label, type, meta: {touched, error, warning}}) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched &&
+        ((error && <span>{error}</span>) ||
+          (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+)
+
+let SubmissionForm = props => {
+  const {handleSubmit, pristine, submitting} = props
+  return (
+    <form>
+      <Field
+        className="field input"
+        name="quantity"
+        component={renderField}
+        type="text"
+      />
+      <Field
+        className="field input"
+        name="quantity"
+        component={renderField}
+        type="text"
+      />
+      <Field
+        className="field input"
+        name="quantity"
+        component={renderField}
+        type="text"
+      />
+    </form>
+  )
+}
+
+const mapState = state => {
+  initialValues: {
   }
 }
+
+SubmissionForm = reduxForm({form: 'submissionForm'})(SubmissionForm)
+SubmissionForm = connect(mapState)(SubmissionForm)
+
+export default SubmissionForm
