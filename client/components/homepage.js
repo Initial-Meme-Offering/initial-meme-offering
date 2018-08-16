e803d8b311b3b254dc2c064a98a8883449a0e42f
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-08-16 11:38:47 -0500

    With homepage images

--- client/components/homepage.js
+++ client/components/homepage.js
@@ -16,35 +16,35 @@ const HomePage = () => {
           <div className="column is-one-third">
             <figure className="image is-square">
               <Link to="/allmemes" component={AllMemes}>
-                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+                <img src="https://i.imgflip.com/2fzph4.jpg" />
               </Link>
             </figure>
           </div>
           <div className="column is-one-third">
             <figure className="image is-square">
               <Link to="/trendingmemes" component={TrendingMemes}>
-                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+                <img src="https://i.imgflip.com/2fzpqs.jpg" />
               </Link>
             </figure>
           </div>
           <div className="column is-one-third">
             <figure className="image is-square">
               <Link to="/indices" component={Indices}>
-                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+                <img src="https://i.imgflip.com/2fzpy0.jpg" />
               </Link>
             </figure>
           </div>
           <div className="column is-one-third">
             <figure className="image is-square">
               <Link to="/searchmemes" component={SearchMemes}>
-                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+                <img src="https://i.imgflip.com/2fzq2b.jpg" />
               </Link>
             </figure>
           </div>
           <div className="column is-one-third">
             <figure className="image is-square">
               <Link to="/purchasememes" component={PurchaseMemes}>
-                <img src="https://www.lifewire.com/thmb/M1ISdSdfLsU36nAuILe3YlFcY1w=/400x400/filters:fill(auto,1)/success-56a9fd1f3df78cf772abee09.jpg" />
+                <img src="https://i.imgflip.com/2fzq6o.jpg" />
               </Link>
             </figure>
           </div>
