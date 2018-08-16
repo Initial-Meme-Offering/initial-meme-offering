91bbeb8391502f2cb4e7dbb8e9595b3c577eee79
Eliot Davis <edavis221@gmail.com>
2018-08-16 11:04:37 -0500

    added back end security measures

--- server/api/index.js
+++ server/api/index.js
@@ -8,7 +8,27 @@ router.use('/transactions', require('./transactions'))
 router.use('/indices', require('./indices'))
 router.use('/memeIndices', require('./meme-indices'))
 router.use('/markethistory', require('./market-history'))
-router.use('/offers', require('./offers'))
+
+function isAdmin(req, res, next) {
+  if (req.user && req.user.dataValues.isAdmin) {
+    next()
+  } else {
+    const error = new Error('get outta here')
+    error.status = 401
+    next(error)
+  }
+}
+
+function isUser(req, res, next) {
+  if (req.user) {
+    next()
+  } else {
+    const error = new Error('get outta here')
+    error.status = 401
+    next(error)
+  }
+}
+router.use('/offers', isUser, require('./offers'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
