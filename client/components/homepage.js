b80da2932bb8017baed9015d274a0d8ea5e1503b
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-08-17 15:20:58 -0500

    Basic layout for homepage and transparent nav

--- client/components/homepage.js
+++ client/components/homepage.js
@@ -10,13 +10,83 @@ import {
 
 const HomePage = () => {
   return (
-    <section className="section">
-      <div className="container">
-        <div className="columns is-multiline is-centered">
-          <h2>Meme Exchange</h2>
+    <div>
+      <section className="hero is-primary is-large is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 id="nav-title" className="title">
+              Memenomics
+            </h1>
+            <h2 className="subtitle">
+              One stop shop for buying, selling, and trading your favorite memes
+            </h2>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <section className="section">
+        <div className="container body-center">
+          <div className="columns">
+            <div className="column">
+              <br />
+              <br />
+              <h1>Buy</h1>
+              <br />
+              <br />
+            </div>
+            <div className="column">
+              <br />
+              <br />
+              <h2>Flipping ticker tape here</h2>
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="home-middle-section" className="section">
+        <div className="container body-center">
+          <div className="columns">
+            <div className="column">
+              <br />
+              <br />
+              <h1>Market trend graph here</h1>
+              <br />
+              <br />
+            </div>
+            <div className="column">
+              <br />
+              <br />
+              <h2>Sell</h2>
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="container body-center">
+          <div className="columns">
+            <div className="column">
+              <br />
+              <br />
+              <h1>Trade</h1>
+              <br />
+              <br />
+            </div>
+            <div className="column">
+              <br />
+              <br />
+              <h2>Handshake icon here</h2>
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
