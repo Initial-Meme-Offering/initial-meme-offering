cf1f2a482bc5bfe5efb478de57c4a7228030f0d1
jgierlach <jegierlach@gmail.com>
2018-08-19 15:09:48 -0500

    pre merge commit

--- client/components/all-memes.js
+++ client/components/all-memes.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 import SmallStockCard from './stock-card-small'
 import {Router, Route, Link} from 'react-router-dom'
 import {connect} from 'react-redux'
-import {} from '../store'
 
 class AllMemes extends Component {
   constructor() {
@@ -17,49 +16,57 @@ class AllMemes extends Component {
     const {allIds, byId} = this.props.memes
     const searchResults = allIds.filter(id => {
       return byId[id].name.toLowerCase().includes(this.state.text.toLowerCase())
-    })
-    console.log('this.state.text', this.state.text)
-    return (
-      <section className="section">
-        <h1 className="is-size-1 has-text-centered">All Memes</h1>
-        <div className="container">
-          <div className="field has-addons">
-            <div className="control">
-              <input
-                id="nav-search"
-                className="input"
-                type="text"
-                placeholder="Search"
-                onChange={event => this.setState({text: event.target.value})}
-              />
-            </div>
-            <div className="control">
-              <button type="button" id="nav-search" className="button is-info">
-                Search
-              </button>
-            </div>
-          </div>
-
-          <div className="columns is-multiline">
-            {searchResults.map(id => (
-              <div key={id} className="column is-4">
-                <SmallStockCard
-                  className=""
-                  memeImage={byId[id].imageUrl}
-                  memeName={byId[id].name}
-                  memeId={id}
+    }) 
+    if (this.props.memes && this.props.transactions) {
+      return (
+        <section className="section">
+          <h1 className="is-size-1 has-text-centered">All Memes</h1>
+          <div className="container">
+            <div className="field has-addons">
+              <div className="control">
+                <input
+                  id="nav-search"
+                  className="input"
+                  type="text"
+                  placeholder="Search"
+                  onChange={event => this.setState({text: event.target.value})}
                 />
               </div>
-            ))}
+              <div className="control">
+                <button
+                  type="button"
+                  id="nav-search"
+                  className="button is-info"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+            <div className="columns is-multiline">
+              {searchResults.map(id => (
+                <div key={id} className="column is-4">
+                  <SmallStockCard
+                    className=""
+                    image={byId[id].imageUrl}
+                    name={byId[id].name}
+                    id={id}
+                    symbol={byId[id].symbol}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
-    )
+        </section>
+      )
+    } else {
+      return <h1>This view did not render</h1>
+    }
   }
 }
 
 const mapState = state => ({
-  memes: state.memes
+  memes: state.memes,
+  transactions: state.transactions
 })
 
 export default connect(mapState, null)(AllMemes)
--- client/routes.js
+++ client/routes.js
@@ -39,11 +39,8 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-<<<<<<< HEAD
         <Route path="/allmemes/:memeId" component={SingleMeme} />
-=======
         <Route path="/submit" component={SubmitMeme} />
->>>>>>> master
         <Route path="/index/:indiceId" component={SingleIndice} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
