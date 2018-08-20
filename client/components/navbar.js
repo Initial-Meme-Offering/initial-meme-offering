751781813a094e957d666c6615ac02ff81540b95
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-08-20 14:29:39 -0500

    Added change

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -9,52 +9,38 @@ const Navbar = ({id, handleLogout, isLoggedIn, isAdmin}) => (
     <div className="container">
       <div className="navbar-menu">
         <div className="navbar-end">
-          {isLoggedIn && (
-            // <div className="field is-grouped">
-            //   <p className="control">
-            //     <a href="/" className="button is-link">
-            //       Home
-            //     </a>
-            //   </p>
-            //   <p className="control">
-            //     <a className="button">Cancel</a>
-            //   </p>
-            //   <p className="control">
-            //     <a className="button is-danger">Delete post</a>
-            //   </p>
-            // </div>
-
-            <div className="field is-grouped">
-              <div id="nav-button" className="navbar-item">
-                <p className="control">
-                  <Link
-                    to="/"
-                    className="button is-link is-info is-hoverable is-outlined"
-                  >
-                    Home
-                  </Link>
-                </p>
-              </div>
-              <div id="nav-button" className="navbar-item">
-                <p className="control">
-                  <Link
-                    to="/allmemes"
-                    className="button is-link is-info is-hoverable is-outlined"
-                  >
-                    All Memes
-                  </Link>
-                </p>
-              </div>
-              <div id="nav-button" className="navbar-item">
-                <p className="control">
-                  <Link
-                    to="/trendingmemes"
-                    className="button is-link is-info is-hoverable is-outlined"
-                  >
-                    Trending
-                  </Link>
-                </p>
-              </div>
+          <div className="field is-grouped">
+            <div id="nav-button" className="navbar-item">
+              <p className="control">
+                <Link
+                  to="/"
+                  className="button is-link is-info is-hoverable is-outlined"
+                >
+                  Home
+                </Link>
+              </p>
+            </div>
+            <div id="nav-button" className="navbar-item">
+              <p className="control">
+                <Link
+                  to="/allmemes"
+                  className="button is-link is-info is-hoverable is-outlined"
+                >
+                  All Memes
+                </Link>
+              </p>
+            </div>
+            <div id="nav-button" className="navbar-item">
+              <p className="control">
+                <Link
+                  to="/trendingmemes"
+                  className="button is-link is-info is-hoverable is-outlined"
+                >
+                  Trending
+                </Link>
+              </p>
+            </div>
+            {isLoggedIn && (
               <div id="nav-button" className="navbar-item is-hoverable">
                 <p className="control">
                   <Link
@@ -71,57 +57,36 @@ const Navbar = ({id, handleLogout, isLoggedIn, isAdmin}) => (
                   <Link to="/manage" className="navbar-item">
                     Manage Account
                   </Link>
-
-                  {/* {isAdmin && (
-              <div>
-                <hr className="navbar-divider" />
-                <NavLink
-                  to="/manage"
-                  className="navbar-item"
-                  activeClassName="is-active"
-                >
-                  Manage
-                </NavLink>
-              </div>
-            )} */}
-
                   <hr className="navbar-divider" />
                   <a href="#" onClick={handleLogout} className="navbar-item">
                     Logout
                   </a>
                 </div>{' '}
               </div>
-            </div>
-          )}
-          {!isLoggedIn && (
-            <div className="navbar-item">
-              <div className="field is-grouped">
+            )}
+
+            {!isLoggedIn && (
+              <div id="nav-button" className="navbar-item">
                 <p className="control">
-                  <Link to="/login" className="button is-link is-success">
+                  <Link
+                    to="/login"
+                    className="button is-link is-info is-outlined"
+                  >
                     Enter
                   </Link>
                 </p>
-                <p className="control">
-                  <Link to="/" className="button is-link is-success">
-                    Home
-                  </Link>
-                </p>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </div>
   </nav>
 )
-// /**
-//  * CONTAINER
-//  */
+
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
-    // isAdmin: !!state.me.isAdmin,
-    //id: state.me.id
   }
 }
 
@@ -135,11 +100,7 @@ const mapDispatch = dispatch => {
 
 export default connect(mapState, mapDispatch)(Navbar)
 
-/**
- * PROP TYPES
- */
 Navbar.propTypes = {
   handleLogout: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
-  // isAdmin: PropTypes.bool.isRequired
 }
