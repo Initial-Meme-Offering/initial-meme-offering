68c9fef3ebc4aa49a392fe2d7ec659a5d33981b0
Kayleen Offringa <kayleen.offringa@gmail.com>
2018-08-22 12:12:10 -0500

    Style changes

--- client/components/auth-form-renders/auth-form-all.js
+++ client/components/auth-form-renders/auth-form-all.js
@@ -3,17 +3,27 @@ import {Field, reduxForm} from 'redux-form'
 import {withRouter} from 'react-router-dom'
 import renderEmailField from './email-field'
 import renderPasswordField from './password-field'
-import renderLoginButton from './login-button';
+import renderLoginButton from './login-button'
 
 let AuthFormAll = props => {
-    const {handleSubmit, name, displayName, error} = props
-    return (
-      <form onSubmit={handleSubmit} name={name}>
-        <Field name="email" component={renderEmailField} />
-        <Field name="password" component={renderPasswordField} />
-        <Field name="button" error={error} displayName={displayName}component={renderLoginButton}/>
-      </form>
-    )
+  const {handleSubmit, name, displayName, error} = props
+  return (
+    <form onSubmit={handleSubmit} name={name}>
+      <br />
+      <br />
+      <Field name="email" component={renderEmailField} />
+      <Field name="password" component={renderPasswordField} />
+      <Field
+        name="button"
+        error={error}
+        displayName={displayName}
+        component={renderLoginButton}
+      />
+      <br />
+      <br />
+      <br />
+    </form>
+  )
 }
 
 AuthFormAll = withRouter(AuthFormAll)
